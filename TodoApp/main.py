7e6cc0a3d92f8c3586e922d57ec3ae253cdd63b1
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-02-22 22:50:56 +0530

    aads endpoint to add new todo

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,11 +1,19 @@
 import models
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+class Todo(BaseModel):
+    title: str
+    description: str
+    priority: int = Field(lt=6, gt=-1)
+    complete: bool
 
 
 def get_db():
@@ -28,3 +36,15 @@ async def get_todo_by_id(todoID: int, db: Session = Depends(get_db)):
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found")
 
+
+@app.post('/', status_code=status.HTTP_201_CREATED)
+async def add_todo(todo:Todo, db: Session = Depends(get_db)):
+    todo_model = models.TodoEntity()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+    return {'message': 'todo added!'}
